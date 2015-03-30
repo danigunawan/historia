365ef0f8ca0d5dce5448e716c978a4c46e0e25e2
Julia <julia.ryanx@gmail.com>
2015-03-31 01:42:06 +1100

    update show view, map and nav

--- app/assets/javascripts/historiaApp.js
+++ app/assets/javascripts/historiaApp.js
@@ -1,12 +1,12 @@
-// var initializeMap = function () {
-//   var $container = $('.map')[0];
+var initializeMap = function () {
+  var $container = $('.map')[0];
 
-//   var mapOptions = {
-//     zoom: 8,
-//     center: new google.maps.LatLng(-34.397, 150.644)
-//   }
-//   var map = new google.maps.Map($container, mapOptions);
-// };
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  }
+  var map = new google.maps.Map($container, mapOptions);
+};
 
 var historiaApp = {
   likePlace: function (event) {
@@ -25,7 +25,7 @@ var historiaApp = {
       $that.removeClass('fa-heart-o unlike').addClass('fa-heart like');
     });
   },
-  
+
   unlikePlace: function (event) {
     event.preventDefault();
     console.log('unlike');
@@ -43,6 +43,12 @@ var historiaApp = {
       $that.removeClass('fa-heart like').addClass('fa-heart-o unlike');
       console.log('Unlike done?')
     });
+  // },
+  // retrieveWikiContent: function() {
+  //   $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', 
+  //     function(json) {
+  //       $('.content').html(json.parse.text); 
+  //   });
   }
 };
 
@@ -50,21 +56,29 @@ $(document).ready(function() {
 
   $('.like').on('click', historiaApp.likePlace);
   $('.unlike').on('click', historiaApp.unlikePlace);
+  // $('.fa-heart').hover(function() {
+  //   $( this ).addClass('fa-heart').removeClass('fa-heart-o');
+  // }, function() {
+  //   $( this ).removeClass('fa-heart').addClass('fa-heart-o');
+  // }
+  // );
 
   // Display signin/signup popup 
   $('.fa-bars').on('click', function() {
     $('.fa-bars').addClass('hide');
     $('.login-overlay').addClass('show');
+    $('.logged-overlay').addClass('show');
   });
 
   // Hide signin/signup popup 
   $('.fa-close').on('click', function() {
     $('.login-overlay').removeClass('show');
+    $('.logged-overlay').removeClass('show');
     $('.fa-bars').removeClass('hide');
   });
 
-
-  // initializeMap();
+  initializeMap();
+  // historiaApp.retrieveWikiContent();
 
 });
 
