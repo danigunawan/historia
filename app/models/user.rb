71456629af7b8615e2b638c769f0767f883cf0ec
Julia <julia.ryanx@gmail.com>
2015-04-14 23:29:54 +1000

    ready account activation and password reset for production

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :likes
   has_many :places, through: :likes
@@ -57,6 +57,16 @@ class User < ActiveRecord::Base
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
   
   # Create user account when signing in using Facebook for the first time
@@ -109,13 +119,18 @@ class User < ActiveRecord::Base
     where("name ilike ? OR email ilike?  ", "%#{query}%", "%#{query}%")
   end
 
-  private
-    def downcase_email
-      self.email = email.downcase
-    end
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  private
+  
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
