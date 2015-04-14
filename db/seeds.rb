afc5d4aed4a05d5287fd5dbc7c5e6fa74c993e15
Julia <julia.ryanx@gmail.com>
2015-04-15 00:29:27 +1000

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 User.destroy_all
-Place.destroy_all
+# Place.destroy_all
 Photo.destroy_all
 Like.destroy_all
 
-user1 = User.create(:name => 'Hamilton', :email => 'hamilton@email.com', :password => 'kittens', :password_confirmation => 'kittens', :avatar => 'http://featuredcreature.com/wp-content/uploads/2014/10/031D8D7F-AC33-4117-B9B0-903957FAF506.jpg')
+user1 = User.create(:name => 'Hamilton', :email => 'hamilton@email.com', :password => 'chicken', :password_confirmation => 'chicken', :avatar => 'http://featuredcreature.com/wp-content/uploads/2014/10/031D8D7F-AC33-4117-B9B0-903957FAF506.jpg', :activation_digest => '3BdBrXeQZSWqFIDRN8cxHA', :activated => true)
 
-user2 = User.create(:name => 'Venus', :email => 'venus@email.com', :password => 'kittens', :password_confirmation => 'kittens', :avatar => 'http://static.boredpanda.com/blog/wp-content/uploads/2014/11/most-popular-cats-venus-21.jpg')
+user2 = User.create(:name => 'Venus', :email => 'venus@email.com', :password => 'chicken', :password_confirmation => 'chicken', :avatar => 'http://static.boredpanda.com/blog/wp-content/uploads/2014/11/most-popular-cats-venus-21.jpg', :activation_digest => '3BdBrXeQZSWqFIDRN8cxHA', :activated => true)
 
-admin = User.create(:name => 'Admin', :email => 'admin@email.com', :password => 'kittens', :password_confirmation => 'kittens', :is_admin => true)
+admin = User.create(:name => 'Admin', :email => 'admin@email.com', :password => 'kittens', :password_confirmation => 'kittens', :is_admin => true, :activation_digest => '3BdBrXeQZSWqFIDRN8cxHA', :activated => true)
 
-place1 = Place.create(:name => 'Pamukkale, Turkey', :latitude => '37.9205', :longitude => '29.121', :fact => 'Pamukkale means "cotton castle" in Turkish', :content => 'Pamukkale, meaning "cotton castle" in Turkish, is a natural site in Denizli Province in southwestern Turkey. The city contains hot springs and travertines, terraces of carbonate minerals left by the flowing water.', :image => 'http://www.turklandtravel.com/wp-content/uploads/pamukkale4.jpg')
+# place1 = Place.create(:name => 'Pamukkale, Turkey', :latitude => '37.9205', :longitude => '29.121', :fact => 'Pamukkale means "cotton castle" in Turkish', :content => 'Pamukkale, meaning "cotton castle" in Turkish, is a natural site in Denizli Province in southwestern Turkey. The city contains hot springs and travertines, terraces of carbonate minerals left by the flowing water.', :image => 'http://www.turklandtravel.com/wp-content/uploads/pamukkale4.jpg')
 
 # place2 = Place.create(:name => 'Borobudur, Indonesia', :latitude => '-7.608', :longitude => '110.204', :fact => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', :content => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore est necessitatibus ratione odio accusamus ut eligendi ipsa illum maiores perferendis magni, alias odit commodi nihil sunt, tempore esse voluptas.', :image => 'http://pixabay.com/get/71306a1d4ce0495499d7/1427722900/borobudur-513465_1280.jpg')
 # place3 = Place.create(:name => 'Prambanan, Indonesia', :latitude => '-7.752222', :longitude => '110.491667', :fact => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', :content => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quod, repellat sed veniam odit, iusto. Voluptatibus aspernatur harum pariatur repellat beatae, similique, dicta est repudiandae vero, ipsam inventore quam non.', :image => 'http://pixabay.com/get/b5b7b9870c8fd2679214/1427723066/temple-598969_1280.jpg')
